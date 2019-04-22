014d73c053734eabfcc579fb9fea49dca06f8924
David Wittman <dwittman@wickr.com>
2019-04-21 23:36:03 -0500

    Retrieve version from __version__ var

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 import setuptools
 
+from requests_random_user_agent import __version__
+
 setuptools.setup(
     name="requests_random_user_agent",
-    version="0.0.1",
+    version=__version__,
     author="David Wittman",
     author_email="david@wittman.com",
+    url="https://github.com/DavidWittman/requests-random-user-agent",
     description="Automatically generate a random User-Agent for the requests library",
     install_requires=['requests>=2.0.1,<3.0.0'],
     packages=setuptools.find_packages(),
