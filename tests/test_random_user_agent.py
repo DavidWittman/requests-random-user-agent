a77c1a6ab86b6e22a9f1c87696c84fe140a3d3e0
David Wittman <dwittman@wickr.com>
2019-04-21 19:45:10 -0500

    Clean up tests

--- tests/test_random_user_agent.py
+++ tests/test_random_user_agent.py
@@ -4,16 +4,19 @@ import requests
 import requests_random_user_agent
 
 class TestRandomUserAgent(unittest.TestCase):
+    def setUp(self):
+        self.session = requests.Session()
+
     def test_user_agent_is_not_default(self):
-        session = requests.Session()
-        user_agent = session.headers['User-Agent']
-        self.assertNotIn("python-requests", user_agent)
+        self.assertNotIn(
+            "python-requests",
+            self.session.headers['User-Agent']
+        )
 
     def test_user_agent_is_random(self):
         # TODO(dw): This will occasionally fail because it can
         # randomly pick the same UA twice
-        session = requests.Session()
-        user_agent_1 = session.headers['User-Agent']
-        session = requests.Session()
-        user_agent_2 = session.headers['User-Agent']
-        self.assertNotEqual(user_agent_1, user_agent_2)
+        self.assertNotEqual(
+            self.session.headers['User-Agent'],
+            requests.Session().headers['User-Agent']
+        )
