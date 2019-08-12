aa0dc3c3c71ee3a4d6f741c7ee9de9a50d15a282
David Wittman <dwittman@wickr.com>
2019-08-12 17:02:30 -0500

    Close USER_AGENTS_FILE after opening
    Setting USER_AGENTS in one line was causing a file descriptor leak
    because we `open` the file but never close it. Just use `with`.

--- requests_random_user_agent/__init__.py
+++ requests_random_user_agent/__init__.py
@@ -3,9 +3,10 @@ import random
 
 import requests.utils
 
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 USER_AGENTS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'useragents.txt')
-USER_AGENTS = open(USER_AGENTS_FILE).read().splitlines()
+with open(USER_AGENTS_FILE) as fh:
+    USER_AGENTS = fh.read().splitlines()
 
 requests.utils.default_user_agent = lambda: random.choice(USER_AGENTS)
