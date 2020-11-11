35fe5864b31ba8ed00df51900506ad4abf014370
David Wittman <dwittman@wickr.com>
2020-11-11 17:41:13 -0600

    Update useragents.txt

--- requests_random_user_agent/__init__.py
+++ requests_random_user_agent/__init__.py
@@ -3,7 +3,7 @@ import random
 
 import requests.utils
 
-__version__ = "2020.10.19"
+__version__ = "2020.11.11"
 
 USER_AGENTS_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'useragents.txt')
 with open(USER_AGENTS_FILE) as fh:
